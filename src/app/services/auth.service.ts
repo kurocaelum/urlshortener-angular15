46e63b177afd2755382f1c860592adcd97a5e99d
Victor <victor_brandao@outlook.com>
2025-05-07 12:13:06 -0300

    feat: loginGuard + auth0/angular-jwt

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, map, Observable, throwError } from 'rxjs';
 
+// Services
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -32,6 +35,17 @@ export class AuthService {
   public logout() {
     localStorage.removeItem('access_token')
     return this.router.navigate([''])
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('access_token')
+
+    if(!token)
+      return false
+
+    const jwtHelper = new JwtHelperService()
+
+    return !jwtHelper.isTokenExpired(token)
   }
 
 }
